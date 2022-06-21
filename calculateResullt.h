44589f9307653d04b46ff2462a6923be0abd6c69
Grzybex <101136184+Grzybex@users.noreply.github.com>
2022-06-21 21:24:37 +0200

    Dodanie calculateResullt.h

---
+++ calculateResullt.h
@@ -0,0 +1,24 @@
+
+
+#ifndef BER_KALKULATOR_CALCULATERESULLT_H
+#define BER_KALKULATOR_CALCULATERESULLT_H
+
+#include <string>
+#include <vector>
+#include <time.h>
+
+
+struct testData
+{
+  std::vector<char> data1;
+  std::vector<char> data2;
+  size_t volume;
+};
+
+testData zaladujZpliku(std::string  path1, std::string  path2);
+std::string timeConverter(int seconds);
+void test_1();
+void test_2();
+void test_3();
+
+#endif //BER_KALKULATOR_CALCULATERESULLT_H

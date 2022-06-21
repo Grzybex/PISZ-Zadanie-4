4bc7f042227fbbdf9f60edbb228b6a9bc06e29db
Grzybex <101136184+Grzybex@users.noreply.github.com>
2022-06-21 21:22:33 +0200

    Dodanie dataFiles.h

---
+++ dataFiles.h
@@ -0,0 +1,11 @@
+
+
+#ifndef BER_KALKULATOR_DATAFILES_H
+#define BER_KALKULATOR_DATAFILES_H
+
+void utwPlikiDlaTestu1();
+void utwPlikiDlaTestu2();
+void utwPlikiDlaTestu3();
+
+
+#endif //BER_KALKULATOR_DATAFILES_H

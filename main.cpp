cc706d6d2a7f8cf70a032a5dd4d9726cd5aa259f
Grzybex <101136184+Grzybex@users.noreply.github.com>
2022-06-21 21:40:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
 
+#include "dataFiles.h"
+#include "calculateResullt.h"
+int main() {
+    utwPlikiDlaTestu1();
+    utwPlikiDlaTestu2();
+    utwPlikiDlaTestu3();
+
+    test_1();
+    test_2();
+    test_3();
+    return 0;
+}
+

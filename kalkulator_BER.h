80961899558375ab07370b94647072e86e80c3be
Grzybex <101136184+Grzybex@users.noreply.github.com>
2022-06-21 21:21:41 +0200

    Dodanie kalkulator_BER.h

---
+++ kalkulator_BER.h
@@ -0,0 +1,15 @@
+
+
+#ifndef BER_KALKULATOR_KALKULATOR_BER_H
+#define BER_KALKULATOR_KALKULATOR_BER_H
+#include<time.h>
+
+
+
+int policzBadBits(char c1, char c2);
+
+
+
+
+
+#endif //BER_KALKULATOR_KALKULATOR_BER_H

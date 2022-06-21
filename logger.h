c0ff9a7b873eaf5460ccb8ed8e993b846b134390
Grzybex <101136184+Grzybex@users.noreply.github.com>
2022-06-21 21:13:45 +0200

    Pierwszy plik

---
+++ logger.h
@@ -0,0 +1,14 @@
+
+
+#ifndef BER_KALKULATOR_LOGGER_H
+#define BER_KALKULATOR_LOGGER_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+void    otworzLoga(std::string fileName);
+void    zapiszLog(std::string msg);
+void    zamknijLog();
+
+#endif BER_KALKULATOR_LOGGER_H
